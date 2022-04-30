9b7591f718eefe1537d2e912b3d2258fc2b9dfa5
Clare Cerullo <jewelofsong@gmail.com>
2022-04-30 10:30:45 -0400

    rollup configured and svelte skeleton built

---
+++ splash/rollup.config.js
@@ -0,0 +1,17 @@
+import svelte from 'rollup-plugin-svelte';
+import resolve from '@rollup/plugin-node-resolve';
+
+export default {
+  input: 'src/main.js',
+  output: {
+    file: 'public/build/bundle.js',
+    format: 'iife',
+    name: 'app'
+  },
+  plugins: [
+    svelte({
+      include: 'src/**/*.svelte'
+    }),
+    resolve({ browser: true }),
+  ]
+}
