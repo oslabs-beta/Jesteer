dcc1c4377e36d68248e2b263e13a686bd7461d38
347Online <38228372+347Online@users.noreply.github.com>
2022-05-02 13:03:40 -0500

    Fix error with getSelectorPath for now

--- extension/static/popup.js
+++ extension/static/popup.js
@@ -21,41 +21,40 @@ btnSnapshot.addEventListener('click', async () => {
   window.close();
 });
 
-// Helper Function to return a Selector Path to the given element
-function getSelectorPath(element) {
-  // TODO: This function is copied verbatim in two place.
-  // This is not DRY.
-  // Maybe a static method on a Class?
-
-  const names = [];
-  while (element.parentNode) {
-    if (element.id) {
-      names.unshift('#' + element.id);
-      break;
-
-    } else {
-
-      if (element === element.ownerDocument.documentElement) names.unshift(element.tagName);
-      else {
-        let e = element;
-        let i = 1;
-
-        while (e.previousElementSibling) {
-          e = e.previousElementSibling;
-          i++;
-        }
-        names.unshift(`${ element.tagName }:nth-child(${ i })`);
-      }
-    }
-    element = element.parentNode;
-  }
-  return names.join(' > ');
-}
-
-/// trying to modularize out getSelectorPath
 
 // Injects the event listeners that select/deselect DOM elements
 function prepareSnapshot() {
+  // Helper Function to return a Selector Path to the given element
+  function getSelectorPath(element) {
+    /// trying to modularize out getSelectorPath
+    // TODO: This function is copied verbatim in two place.
+    // This is not DRY.
+    // Maybe a static method on a Class?
+  
+    const names = [];
+    while (element.parentNode) {
+      if (element.id) {
+        names.unshift('#' + element.id);
+        break;
+  
+      } else {
+  
+        if (element === element.ownerDocument.documentElement) names.unshift(element.tagName);
+        else {
+          let e = element;
+          let i = 1;
+  
+          while (e.previousElementSibling) {
+            e = e.previousElementSibling;
+            i++;
+          }
+          names.unshift(`${ element.tagName }:nth-child(${ i })`);
+        }
+      }
+      element = element.parentNode;
+    }
+    return names.join(' > ');
+  }
 
   //Helper functions for selecting and deselecting elements
   const select = e => e.target.setAttribute('___jesteer___highlight', '');
