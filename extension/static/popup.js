ea087a14a0b7309af9bdb8446b4e6d083c2132e9
347Online <38228372+347Online@users.noreply.github.com>
2022-05-04 15:49:38 -0500

    Current test now persists if the popup or browser is closed

--- extension/static/popup.js
+++ extension/static/popup.js
@@ -1,3 +1,9 @@
+document.addEventListener('DOMContentLoaded', async () => {
+  await chrome.storage.local.get('currentTest', ({ currentTest }) => {
+    codegen.value = currentTest ?? '';
+  });
+});
+
 btnCopy.addEventListener('click', async () => {
   const code = document.querySelector('#codegen').value;
   await navigator.clipboard.writeText(code);
